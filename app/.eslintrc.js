3ef6fc5ba319f8b7ee0ccf6e378c9a72247391c6
Jason Cramer <cramerj1@rose-hulman.edu>
2022-09-29 19:35:12 -0400

    Fixed github actions

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,17 +1,18 @@
-import { resolve } from "path";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require('path')
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: [
-      resolve(__dirname, "./tsconfig.json"),
-      resolve(__dirname, "./tsconfig.eslint.json"),
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: [
+            resolve(__dirname, './tsconfig.json'),
+            resolve(__dirname, './tsconfig.eslint.json'),
+        ],
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-};
+}
