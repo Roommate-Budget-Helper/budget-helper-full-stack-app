c2af1a974112f57a129c87d2cdbf0f9c6bdf8486
Jason Cramer <cramerj1@rose-hulman.edu>
2022-12-07 18:08:36 +0000

    Started login/signup

---
+++ app/src/components/fieldinput/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface FieldInputProps {
+    value: string;
+    placeholder: string;
+    type: React.HTMLInputTypeAttribute;
+    onChange: React.ChangeEventHandler<HTMLInputElement>
+}
+
+const FieldInput: React.FC<Partial<FieldInputProps>> = (props) => {
+    return (
+    <input 
+        className="bg-evergreen-60 w-96 my-4 text-xl py-2 px-4 rounded-lg" 
+        type={props.type} value={props.value} 
+        onChange={props.onChange}
+        placeholder={props.placeholder}/>);
+};
+
+export default FieldInput;
---
+++ app/src/pages/login.tsx
@@ -0,0 +1,57 @@
+import Button from "@components/button";
+import FieldInput from "@components/fieldinput";
+import type { NextPage } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
+
+
+const LoginPage: NextPage = () => {
+    const [username, setUsername]= useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    return (<>
+        <Head>
+            <title>RBH Login</title>
+            <meta
+                name="description"
+                content="Login page of Roommate Budget Helper" />
+        </Head>
+        <div className="body flex flex-col text-center">
+            <div className="headertext-center pt-4">
+                <h1 className="text-5xl font-bold text-evergreen-100">Welcome Back 👋</h1>
+                <p className="text-xl font-light">We are excited to have you back but to get started please login!</p>
+            </div>
+            <div className="form-area flex flex-col justify-between items-center ">
+                <FieldInput 
+                    type="text" 
+                    value={username} 
+                    onChange={e => setUsername(e.currentTarget.value)}
+                    placeholder="Username or Email" />
+                 <FieldInput 
+                    type="password" 
+                    value={password} 
+                    onChange={e => setPassword(e.currentTarget.value)}
+                    placeholder="Password" />
+                <Button 
+                    classNames="bg-evergreen-80 text-dorian"
+                    value="Login" 
+                    type="button" />
+            </div>
+            <div className="divider my-4">
+                <hr/>
+            </div>
+            <div className="providers">
+                <Button
+                    classNames="bg-dorian text-black"
+                    value="Google"
+                    type="button" />
+            </div>
+            <div className="actions">
+                <p>Don't have an account?</p>
+                <p>Forgot your password?</p>
+
+            </div>
+        </div>
+    </>);
+};
+
+export default LoginPage;
