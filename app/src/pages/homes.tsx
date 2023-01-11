de6da7ab93f75b0e1e635acdef24096375094793
rhit-daviscl <daviscl@rose-hulman.edu>
2023-01-11 14:34:31 -0500

    Added create home to homes tsc, but not completed or tested

--- app/src/pages/homes.tsx
+++ app/src/pages/homes.tsx
@@ -6,21 +6,116 @@ import React from "react";
 import Image from "next/image";
 import { trpc } from "utils/trpc";
 import Navbar from "@components/navbar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const HomesPage: NextPage = () => {
 
-    function showCreateHomeForm() {
-        const body = document.getElementById("core");
-        if (body != null) {
-            body.innerHTML =
-                "<div>Create Home</div><br></br><form method='post' onSubmit={createHome}>" +
-                "<div><Image src='/images/apartment.png' alt='Home' width='50px' height='50px' /> Upload Home Avatar</div>" +
-                "<FieldInput type='text' name='name' placeholder='Home Name' /> <br></br>" +
-                "<FieldInput type='text' name='address' placeholder='Location' /> <br></br>" +
-                "<FieldInput type='text' name='address' placeholder='' /> <br></br>" +
-                "<Button classNames='bg-evergreen-80 text-dorian' value='' type='submit' /> <br></br> </form>";
-            // "<div id='homeName'>Home 1</div> <div id='address'>Insert Address Here!</div>";
-        }
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    if (status == "unauthenticated") {
+        router.push("/login");
+        return <div></div>;
+    }
+    const createHome = trpc.useMutation(["home.createHome"], {
+        onError: (error) => {
+            console.error(error);
+        },
+        onSuccess: (home) => {
+            console.log("Home %s created successfully", home.name);
+            // Add userid and homeid to the occupies table
+            if(!session?.user){
+                return;
+            }
+            addUserToHome.mutate({
+                homeId: home.id,
+                userId: session.user.id
+            });
+        },
+    });
+    const addUserToHome = trpc.useMutation(["occupies.addUserToHome"], {
+        onError: (error) => {
+            console.error(error);
+        },
+        onSuccess: (_, { userId, homeId }) => {
+            console.log(
+                "User %s add to Home %s created successfully",
+                userId,
+                homeId
+            );
+        },
+    });
+
+    const onCreateHome = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+
+        createHome.mutate({
+            image: form.elements["image"].value,
+            name: form.elements["name"].value,
+            address: form.elements["address"].value,
+        });
+    };
+
+    let createAHome = false;
+
+    function createAHomePressed() {
+        createAHome = true;
+    }
+    
+    if(createAHome) {
+        return (
+            <>
+                <Head>
+                    <title>RBH Homes Page</title>
+                    <meta
+                        name="description"
+                        content="Homes page of Roommate budget helper"
+                    />
+                </Head>
+                <div className="body flex flex-col text-center">
+                    <div id="core">
+                        <div className="form-area flex flex-col justify-between items-center ">
+                            <div>Create Home</div>
+                            <br></br>
+                            <form method="post" onSubmit={onCreateHome}>
+                                {/* TODO: Give it a cute image uploader */}
+                                <FieldInput
+                                    type="text"
+                                    name="image"
+                                    placeholder="Image"
+                                ></FieldInput>
+                                <br></br>
+                                <FieldInput
+                                    type="text"
+                                    name="name"
+                                    placeholder="Home Name"
+                                />
+                                <br></br>
+                                <FieldInput
+                                    type="text"
+                                    name="address"
+                                    placeholder="Address"
+                                />
+                                <br></br>
+                                {/* <FieldInput
+                                    type="text"
+                                    name=""
+                                    placeholder=""
+                                /> */}
+                                <br></br>
+                                <Button
+                                    classNames="bg-evergreen-80 text-dorian"
+                                    value="Create"
+                                    type="submit"
+                                />
+                                <br></br>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
     }
 
     return (
@@ -50,7 +145,7 @@ const HomesPage: NextPage = () => {
                             height="35px"
                         />
                     </a>
-                    <a className="active center" href="/createhome">
+                    <a className="active center" onClick={createAHomePressed}>
                         <Image
                             src="/images/add_home.png"
                             alt="Home"
