101bd454074076ea1541b65d2c646d17aaa2b829
Charlie Cryer <cryerca@rose-hulman.edu>
2023-01-18 16:33:04 +0000

    merged

---
+++ app/jest.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    clearMocks: true,
+    coverageProvider: "v8",
+    preset: "ts-jest/presets/js-with-ts",
+    setupFiles: ["dotenv/config"],
+  };
