2dcbd0210422805c9209a9c295c5d789e187a3b3
Charlie Cryer <cryerca@rose-hulman.edu>
2023-01-25 08:42:24 +0000

    fixed linting error for using <a> instead of <Link>

--- app/src/components/navbar/index.tsx
+++ app/src/components/navbar/index.tsx
@@ -1,49 +1,50 @@
 import Image from "next/image";
+import Link from "next/link";
 
 // TODO: WIP fix styles
 const Navbar: React.FC = () => {
     return (
         <div className="overflow-hidden rounded-b-lg p-4 sm:bottom-0 sm:w-full sm:fixed ">
-        <a className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/bill">
+        <Link className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/bill">
             <Image
                 src="/images/bills.png"
                 alt="Home"
                 width="35px"
                 height="35px"
             />
-        </a>
-        <a className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="#">
+        </Link>
+        <Link className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="#">
             <Image
                 src="/images/thing.png"
                 alt="Home"
                 width="35px"
                 height="35px"
             />
-        </a>
-        <a className="text-white bg-green-400 w-1/5 m-auto h-full float-left text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/createhome">
+        </Link>
+        <Link className="text-white bg-green-400 w-1/5 m-auto h-full float-left text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/createhome">
             <Image
                 src="/images/add_home.png"
                 alt="Home"
                 width="60px"
                 height="60px"
             />
-        </a>
-        <a className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/notification">
+        </Link>
+        <Link className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/notification">
             <Image
                 src="/images/notifications.png"
                 alt="Home"
                 width="35px"
                 height="35px"
             />
-        </a>
-        <a className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/homes">
+        </Link>
+        <Link className="w-1/5 m-auto h-full float-left bg-gray-700 text-white text-center p-3 no-underline text-lg hover:bg-gray-100 hover:text-black" href="/homes">
             <Image
                 src="/images/user.png"
                 alt="Home"
                 width="35px"
                 height="35px"
             />
-        </a>
+        </Link>
     </div>
     );
 };
