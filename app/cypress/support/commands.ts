4d26eb1a9f328881cc90ea0c1dff17b561b446dc
Jason Cramer <cramerj1@rose-hulman.edu>
2023-02-15 17:19:07 +0000

    Merged with main

---
+++ app/cypress/support/commands.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+import { Email, InboxDto } from "mailslurp-client";
+
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      createInbox(): Promise<InboxDto>,
+      waitForEmail(inboxId: string): Promise<Email>
+    }
+  }
+}
