d9c00c5ebaa767aac18c24bb8cad1def61537e76
David Purdy <Purdydj@Rose-Hulman.edu>
2023-04-04 19:59:10 -0400

    Refactor frontend (#9)
    * image file upload input
    
    * Money, Image, and Date Picker
    
    * adjust width size for smaller mobile displays
    
    * responsive and splitting field inputs validation
    
    * cast to number and remove console log

--- app/src/pages/billing.tsx
+++ app/src/pages/billing.tsx
@@ -50,51 +50,80 @@ const BillingPage: NextPage = () => {
                 <title>RBH Bills</title>
                 <meta name="description" content="Billing and Charges" />
             </Head>
-            <div className="body flex flex-col text-center">
-                <Navbar/>
-                <h1 className="text-5xl my-10 font-bold text-evergreen-100">Billing</h1>
-                  <div className="form-area flex flex-col justify-between items-center">
-                    <div className="bg-slate-200 py-10 px-10 rounded-xl">
+            <Navbar />
+            <div className="body flex flex-col text-center text-2xl p-5">
+                <h1 className="text-5xl my-10 font-bold text-evergreen-100">
+                    Billing
+                </h1>
+                <div className="form-area flex flex-col justify-between items-center">
+                    <div className="py-10 px-10 rounded-xl hover:shadow-xl">
                         <Button
                             classNames="bg-evergreen-80 text-dorian"
                             value="Send Charge"
                             type="button"
                             onClick={() => {
-                              router.push("/createcharge");
+                                router.push("/createcharge");
                             }}
                         />
-                        <h2 className="text-3xl mt-5 font-bold text-evergreen-100">Charges to Pay</h2>
+                        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                        <h2 className="text-3xl mt-5 font-bold text-evergreen-100">
+                            Charges to Pay
+                        </h2>
                         {unpaidCharges && unpaidCharges.length > 0 ? (
                             <div>
                                 {unpaidCharges.map((charge) => (
                                     <div
                                         key={charge.chargeId}
-                                        className="bg-slate-600 justify-items-center mx-106 my-10 p-3 rounded-xl text-dorian text-base"
+                                        className="sm:bg-transparent md:bg-slate-600 items-center mx-10 my-10 p-3 rounded-xl flex flex-col sm:text-black md:text-dorian text-base justify-between"
                                     >
                                         {charge.chargeUser.name && (
-                                          <div className="rounded-full bg-evergreen-80 w-24 h-24 flex items-center justify-center">
-                                            {charge.chargeUser.image ?
-                                              <Image src={charge.chargeUser.image} alt={charge.chargeUser.name} width="64px" height="64px" />
-                                              :<p>{charge.chargeUser.name.split(" ").reduce((a,c) => {
-                                                a += c[0];
-                                                return a;
-                                            }, "")}</p>}
+                                            <div className="rounded-full bg-evergreen-80 w-24 h-24 flex items-center flex-col justify-center">
+                                                {charge.chargeUser.image ? (
+                                                    <Image
+                                                        src={
+                                                            charge.chargeUser
+                                                                .image
+                                                        }
+                                                        alt={
+                                                            charge.chargeUser
+                                                                .name
+                                                        }
+                                                        width="64px"
+                                                        height="64px"
+                                                    />
+                                                ) : (
+                                                    <p>
+                                                        {charge.chargeUser.name
+                                                            .split(" ")
+                                                            .reduce((a, c) => {
+                                                                a += c[0];
+                                                                return a.toUpperCase();
+                                                            }, "")}
+                                                    </p>
+                                                )}
                                             </div>
                                         )}
                                         <br></br>
-                                        <p>Charger: {charge.chargeUser.name} </p>
+                                        <p className="mt-5">
+                                            Charger: {charge.chargeUser.name}
+                                        </p>
                                         <p>Description: {charge.comment} </p>
-                                        <p>Amount Before Splitting: ${charge.amountBeforeSplit}</p>
+                                        <p>
+                                            Amount Before Splitting: $
+                                            {charge.amountBeforeSplit}
+                                        </p>
                                         <p>Amount Owed: ${charge.amount}</p>
-                                        <p>Due Date: {charge.dueDate.toDateString()}</p>
-                                        <br></br>
+                                        <p className="mb-5">
+                                            Due Date:{" "}
+                                            {charge.dueDate.toDateString()}
+                                        </p>
                                         <div className="flex space-x-5">
                                             <Button
                                                 classNames="bg-evergreen-80"
                                                 value="Send Payment"
                                                 onClick={handlePayCharge(
                                                     true,
-                                                    charge.chargeId,
+                                                    charge.chargeId
                                                 )}
                                             />
                                         </div>
@@ -102,32 +131,42 @@ const BillingPage: NextPage = () => {
                                 ))}
                             </div>
                         ) : (
-                            <div>You have no unpaid charges.</div>
+                            <div className="sm:bg-transparent items-center mx-10 p-3 rounded-xl flex flex-col sm:text-black text-xl text-bold justify-between">
+                                You have no unpaid charges.
+                            </div>
                         )}
 
                         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
-                        <h2 className="text-3xl mt-5 font-bold text-evergreen-100">Charges to Confirm</h2>
+                        <h2 className="text-3xl mt-5 font-bold text-evergreen-100">
+                            Charges to Confirm
+                        </h2>
                         {unconfirmedCharges && unconfirmedCharges.length > 0 ? (
                             <div>
                                 {unconfirmedCharges.map((charge) => (
                                     <div
                                         key={charge.chargeId}
-                                        className="bg-slate-600 mx-106 my-10 p-3 rounded-xl text-dorian text-base"
+                                        className="sm:bg-transparent md:bg-slate-600 items-center mx-10 my-10 p-3 rounded-xl flex flex-col sm:text-black md:text-dorian text-base justify-between"
                                     >
                                         <p>From: {charge.receiveUser.name} </p>
                                         <p>Description: {charge.comment} </p>
-                                        <p>Amount Before Splitting: ${charge.amountBeforeSplit}</p>
+                                        <p>
+                                            Amount Before Splitting: $
+                                            {charge.amountBeforeSplit}
+                                        </p>
                                         <p>Amount Paid: ${charge.amount}</p>
-                                        <p>Date Paid: {charge.paidDate?.toDateString()}</p>
-                                        
+                                        <p>
+                                            Date Paid:
+                                            {charge.paidDate?.toDateString()}
+                                        </p>
+
                                         <div className="flex space-x-5">
                                             <Button
                                                 classNames="bg-evergreen-80"
                                                 value="Confirm Payment"
                                                 onClick={confirmChargePaid(
                                                     true,
-                                                    charge.chargeId,
+                                                    charge.chargeId
                                                 )}
                                             />
                                         </div>
@@ -135,10 +174,16 @@ const BillingPage: NextPage = () => {
                                 ))}
                             </div>
                         ) : (
-                            <div>You have no charges pending approval.</div>
+                            <div className="sm:bg-transparent items-center mx-10 p-3 rounded-xl flex flex-col sm:text-black text-xl text-bold justify-between">
+                                You have no charges pending approval.
+                            </div>
                         )}
 
-                        {error &&<p className="text-xl font-light text-red-600">{error}</p>}
+                        {error && (
+                            <p className="text-xl font-light text-red-600">
+                                {error}
+                            </p>
+                        )}
                     </div>
                 </div>
             </div>
